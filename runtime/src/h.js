1e2a3d3521cd895d08e6d14e57c04434ded7e6c5
danish-mehmud <danish.mehmood@strmarketplace.com>
2025-10-05 14:42:50 -0700

    done with h.js

---
+++ runtime/src/h.js
@@ -0,0 +1,36 @@
+import { withoutNulls } from "./utils/array";
+
+export const DOM_TYPES = {
+  TEXT: "text",
+  ELEMENT: "element",
+  FRAGMENT: "fragment",
+};
+
+//following h function is equivalent to react createElement function
+//and it creates a virtual dom node out of given info
+
+function h(tag, props = {}, children = []) {
+  return {
+    tag,
+    props,
+    children: mapTextNodes(withoutNulls(children)),
+    type: DOM_TYPES.ELEMENT,
+  };
+}
+
+function mapTextNodes(children) {
+  return children.map((child) =>
+    typeof child === "string" ? hString(child) : child
+  );
+}
+
+export function hString(str) {
+  return { type: DOM_TYPES.TEXT, value: str };
+}
+
+export function hFragment(vNodes) {
+  return {
+    type: DOM_TYPES.FRAGMENT,
+    children: mapTextNodes(withoutNulls(vNodes)),
+  };
+}
